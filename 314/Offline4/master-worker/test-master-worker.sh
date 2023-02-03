413daca769e515a663529ea687d5be8cf59b0847
Shamim2601 <shamim2601@gmail.com>
2023-02-03 15:57:49 +0600

    314 off 4 initial

---
+++ 314/Offline4/master-worker/test-master-worker.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# script takes 4 arguments that are given to the master worker program
+
+gcc -o master-worker master-worker.c -lpthread
+./master-worker $1 $2 $3 $4 > output    ##total_items #max_buf_size #workers #masters
+#awk -f check.awk MAX=$1 output
